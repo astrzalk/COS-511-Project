d864bb0cc2ab4bc78d662eb0327492fbc8e4a177
Ethan Tseng <ethan.tseng123@gmail.com>
2019-04-20 17:02:29 -0400

    Added Isolet data set

--- MultiBoost/gen_weights.py
+++ MultiBoost/gen_weights.py
@@ -1,5 +1,8 @@
 import os
 
 for i in range(31):
-    os.system('./multiboost --fileformat arff --traintest pendigitsTrain.arff pendigitsTest.arff {} --verbose 5 --learnertype SingleStumpLearner --outputinfo resultSingleStump.dta ham --shypname shypSingleStump.xml --weights weight.txt'.format(i))
-    os.system('mv weight.txt ../data/pendigits/single_stump/weights/weight_{}.txt'.format(i))
+    #os.system('./multiboost --fileformat arff --traintest pendigitsTrain.arff pendigitsTest.arff {} --verbose 5 --learnertype SingleStumpLearner --outputinfo resultSingleStump.dta ham --shypname shypSingleStump.xml --weights weight.txt'.format(i))
+    os.system('./multiboost --fileformat arff --traintest isoletTrain.arff isoletTest.arff {} --verbose 5 --learnertype SingleStumpLearner --outputinfo resultSingleStump.dta ham --shypname shypSingleStump.xml --weights weight.txt'.format(i))
+
+    #os.system('mv weight.txt ../data/pendigits/single_stump/weights/weight_{}.txt'.format(i))
+    os.system('mv weight.txt ../data/isolet/single_stump/weights/weight_{}.txt'.format(i))
--- MultiBoost/get_vote_vectors.py
+++ MultiBoost/get_vote_vectors.py
@@ -7,9 +7,11 @@ multiboost = tree.getroot()
 for t in range(1,31):
     print(multiboost[t].attrib['iter'])
     v = []
-    for l in range(1,11):
+    for l in range(1,27):
+    #for l in range(1,11):
         print(int(multiboost[t][2][l].text))
         v.append(int(multiboost[t][2][l].text))
 
     np_v = np.array(v)
-    np.save('../data/pendigits/single_stump/vote_vectors/v_{}'.format(t), np_v)
+    np.save('../data/isolet/single_stump/vote_vectors/v_{}'.format(t), np_v)
+    #np.save('../data/pendigits/single_stump/vote_vectors/v_{}'.format(t), np_v)
