5c2ead1f9c6e44737383af8fbf6341a1d6a1a950
Ethan Tseng <ethan.tseng123@gmail.com>
2019-04-21 15:26:26 -0400

    Added binary test weights

--- MultiBoost/gen_weights.py
+++ MultiBoost/gen_weights.py
@@ -2,7 +2,10 @@ import os
 
 for i in range(31):
     #os.system('./multiboost --fileformat arff --traintest pendigitsTrain.arff pendigitsTest.arff {} --verbose 5 --learnertype SingleStumpLearner --outputinfo resultSingleStump.dta ham --shypname shypSingleStump.xml --weights weight.txt'.format(i))
-    os.system('./multiboost --fileformat arff --traintest isoletTrain.arff isoletTest.arff {} --verbose 5 --learnertype SingleStumpLearner --outputinfo resultSingleStump.dta ham --shypname shypSingleStump.xml --weights weight.txt'.format(i))
+    #os.system('./multiboost --fileformat arff --traintest isoletTrain.arff isoletTest.arff {} --verbose 5 --learnertype SingleStumpLearner --outputinfo resultSingleStump.dta ham --shypname shypSingleStump.xml --weights weight.txt'.format(i))
+    os.system('./multiboost --fileformat arff --traintest good_data/pendigitsTrainbinary.arff good_data/pendigitsTestbinary.arff {} --verbose 5 --learnertype SingleStumpLearner --outputinfo resultSingleStump.dta edg --shypname shypSingleStump.xml --weights weight.txt'.format(i))
+
 
     #os.system('mv weight.txt ../data/pendigits/single_stump/weights/weight_{}.txt'.format(i))
-    os.system('mv weight.txt ../data/isolet/single_stump/weights/weight_{}.txt'.format(i))
+    #os.system('mv weight.txt ../data/isolet/single_stump/weights/weight_{}.txt'.format(i))
+    os.system('mv weight.txt ../data/pendigits_binary/single_stump/weights/weight_{}.txt'.format(i))
--- data/utils/make_weights_numpy.py
+++ data/utils/make_weights_numpy.py
@@ -14,7 +14,8 @@ def save_csv_as_np(data_path, save_path, filename):
     :returns: None, two saved .npy files at save_path
     """
     #cols_to_use = [0,1,2,3,4,5,6,7,8,9]
-    cols_to_use = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+    #cols_to_use = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+    cols_to_use = [0,1]
     df = pd.read_csv(data_path + filename + '.txt', sep=';', usecols = cols_to_use, header=None) 
     np_array = df.values # Convert to numpy array
 
@@ -23,7 +24,8 @@ def save_csv_as_np(data_path, save_path, filename):
 
 def main():
     #weight_path = '../pendigits/single_stump/weights/'
-    weight_path = '../isolet/single_stump/weights/'
+    #weight_path = '../isolet/single_stump/weights/'
+    weight_path = '../pendigits_binary/single_stump/weights/'
     for i in range(31):
         save_csv_as_np(weight_path, weight_path, 'weight_{}'.format(i))
 
